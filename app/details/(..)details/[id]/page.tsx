dcac40223d8f98d34911f9478dc7486b72fffb73
Mariam Jubril <mariamjubril29@gmail.com>
2024-01-14 10:27:00 +0100

    secondd

--- app/details/(..)details/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import Modal from "@/components/modal";
-import Specialist from '@/data/sp.json'
-export default function DetailsModal() {
-    params: {id}
-    {
-        params: {id: string}
-    }
-}
